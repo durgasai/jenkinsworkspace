342ce2a334754ce02f30e68f0aae2a28ffc04e34
durgasai <venkatasai.947@gmail.com>
2020-04-19 15:27:38 -0400

    commented properties block

--- workbench/builds/builds_wb.groovy
+++ workbench/builds/builds_wb.groovy
@@ -2,9 +2,9 @@ pipelineJob('WB_DEV_MVN_Build'){
     logRotator {
         numToKeep(5)
     }
-    properties {
-        githubProjectUrl('https://github.com/durgasai/mvnproject.git')
-    }
+    //properties {
+    //    githubProjectUrl('https://github.com/durgasai/mvnproject.git')
+    //}
     defintion {
         cps {
             script(readFileFromWorkspace('./workbench/builds/pipeline_scripts/wb_mvn_project.groovy'))

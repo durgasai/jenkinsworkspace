2780825574e9a66529e5e13eff53b2f781826d9e
durgasai <venkatasai.947@gmail.com>
2020-04-19 15:30:03 -0400

    typo error definition is resolved

--- workbench/builds/builds_wb.groovy
+++ workbench/builds/builds_wb.groovy
@@ -2,10 +2,10 @@ pipelineJob('WB_DEV_MVN_Build'){
     logRotator {
         numToKeep(5)
     }
-    //properties {
-    //    githubProjectUrl('https://github.com/durgasai/mvnproject.git')
-    //}
-    defintion {
+    properties {
+        githubProjectUrl('https://github.com/durgasai/mvnproject.git')
+    }
+    definition {
         cps {
             script(readFileFromWorkspace('./workbench/builds/pipeline_scripts/wb_mvn_project.groovy'))
             sandbox()

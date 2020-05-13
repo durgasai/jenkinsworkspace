ff0d5e305ff55d7f01dd564b8271e962571a4428
durgasai <venkatasai.947@gmail.com>
2020-05-13 16:29:01 -0400

    added plotBuilddata

--- workbench/builds/builds_wb.groovy
+++ workbench/builds/builds_wb.groovy
@@ -1,4 +1,22 @@
 pipelineJob('WB_Dev_MVN_Build'){
+    publishers {
+        plotBuildData {
+            plot('Exciting plots', 'excitement.csv') {
+                title('X vs Y')
+                yAxis('Y')
+                numberOfBuilds(42)
+                useDescriptions()
+                keepRecords()
+                excludeZero()
+                logarithmic()
+                yAxisMinimum(10.0)
+                yAxisMaximum(100.0)
+                propertiesFile('my_data.properties') {
+                    label('Builds')
+                }
+            }
+        }
+    }
     logRotator {
         numToKeep(5)
     }
@@ -8,7 +26,6 @@ pipelineJob('WB_Dev_MVN_Build'){
     triggers {
         githubPush()
     }
-
     definition {
         cps {
             script(readFileFromWorkspace('./workbench/builds/pipeline_scripts/wb_mvn_project.groovy'))

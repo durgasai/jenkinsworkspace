28d579aaa3d74ec1322eca2093c07d6ddd61df0e
durgasai <venkatasai.947@gmail.com>
2020-05-13 16:02:19 -0400

    added !!!

--- workbench/builds/pipeline_scripts/wb_mvn_project.groovy
+++ workbench/builds/pipeline_scripts/wb_mvn_project.groovy
@@ -9,7 +9,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh 'echo Hello World!'
+                sh 'echo Hello World!!!!!'
                 sh 'ls'
             }
         }

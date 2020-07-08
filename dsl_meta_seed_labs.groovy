a6e577219427e003ed40b1f01fda48049943199f
durgasai <venkatasai.947@gmail.com>
2020-07-08 18:08:51 -0400

    added scriptapproval in seperate job

--- dsl_meta_seed_labs.groovy
+++ dsl_meta_seed_labs.groovy
@@ -20,13 +20,5 @@
              removeViewAction('DELETE')
          }
      }
-     definition {
-         cps {
-             def pipelinescript = readFileFromWorkspace('dsl_meta_seed_labs.groovy')
-             script(pipelinescript)
-             def scriptApproval = Jenkins.instance.getExtensionList('org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval')[0]
-             scriptApproval.approveScript(scriptApproval.hash(pipelinescript, 'groovy'))
-         }
-     }
  }
 
--- scriptapproval.groovy
+++ scriptapproval.groovy
@@ -1,8 +1,11 @@
-definition {
-    cps {
-        def pipelinescript = readFileFromWorkspace('...*.groovy')
-        script(pipelinescript)
-        def scriptApproval = Jenkins.instance.getExtensionList('org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval')[0]
-        scriptApproval.approveScript(scriptApproval.hash(pipelinescript, 'groovy'))
-    }
-}
+job('script_approval')
+        {
+            definition {
+                cps {
+                    def pipelinescript = readFileFromWorkspace('dsl_meta_seed_labs.groovy')
+                    script(pipelinescript)
+                    def scriptApproval = Jenkins.instance.getExtensionList('org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval')[0]
+                    scriptApproval.approveScript(scriptApproval.hash(pipelinescript, 'groovy'))
+                }
+            }
+        }

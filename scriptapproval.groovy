b96f0773c7c4168e9b5e6b5bda655f2a2a74c97e
durgasai <venkatasai.947@gmail.com>
2020-07-08 17:36:04 -0400

    added scriptapproval

--- scriptapproval.groovy
+++ scriptapproval.groovy
@@ -2,6 +2,7 @@ definition {
     cps {
         def pipelinescript = readFileFromWorkspace('...*.groovy')
         script(pipelinescript)
-        def scriptApproval = Jenkins.instance.get
+        def scriptApproval = Jenkins.instance.getExtensionList('org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval')[0]
+        scriptApproval.approveScript(scriptApproval.hash(pipelinescript, 'groovy'))
     }
 }
